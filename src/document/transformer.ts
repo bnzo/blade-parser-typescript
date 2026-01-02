88ddccff741fa9d0b487d40eef9f0099019e2943
bnzo <17174973+bnzo@users.noreply.github.com>
2026-01-02 13:56:27 +0100

    Remove commented-out code for expression parameters in Transformer class

--- src/document/transformer.ts
+++ src/document/transformer.ts
@@ -1218,6 +1218,10 @@ export class Transformer {
             component.parameters.forEach((param) => {
                 if (param.type == ParameterType.Parameter) {
                     if (param.isExpression) {
+                    //     const expressionSlug = this.makeSlug(param.content.length);
+                    //     this.registerExpressionParameter(expressionSlug, param);
+                    //     value += expressionSlug + ' ';
+                    // } else {
                         value += param.content + ' ';
                     }
                 } else if (param.type == ParameterType.Attribute) {
